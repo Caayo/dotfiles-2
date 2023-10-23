3306f0ee600de9802fbd3bdd4afa3ed3cd582ca2
Caayo <81698096+Caayo@users.noreply.github.com>
2023-10-22 20:09:11 -0500

    Delete dotfiles directory

--- dotfiles/instructions.md
+++
@@ -1,6 +0,0 @@
-# Git
-- git init
-- make sure branch is main
-- git remote add [//your github url]
-- git pull origin master
-- git config credential.helper store
--- dotfiles/symbolic.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Template for symbolic links
-#ls -sf $HOME/.dotfiles/[insert file location/name] $HOME/[insert file location/name]
-
-
-ls -sf $HOME/.dotfiles/.bashrc $HOME/.bashrc
